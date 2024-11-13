5edfe99c98cc7f444890362418c5a941125f69ca
Mamahali <mamahalimatela74@gmail.com>
2024-11-13 18:36:05 +0200

    Updates

---
+++ precache-manifest.67617f4c39b6441e4b223af047c782a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc3b3d8aa628d6f2bedabff0fd43036",
+    "url": "/Matela/index.html"
+  },
+  {
+    "revision": "44fcfe683b47b1c08314",
+    "url": "/Matela/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbc20ca1956509734a3e",
+    "url": "/Matela/static/js/2.1c6a6bbb.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/Matela/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "44fcfe683b47b1c08314",
+    "url": "/Matela/static/js/main.90c1152a.chunk.js"
+  },
+  {
+    "revision": "8ba9566d876869ccd5a0",
+    "url": "/Matela/static/js/runtime~main.989b2e8f.js"
+  }
+]);
